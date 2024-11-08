a6c455cccf5e1fe5a0e2f65e436204e9026a076b
hamza <hamzapratama000@gmail.com>
2024-11-08 12:21:41 +0700

    fix dashboard bug

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,28 +4,29 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useUser, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+
 function Header() {
   const { user, isSignedIn } = useUser();
   return (
     <div className="p-5 flex justify-between items-center border shadow-sm">
       <div className="flex flex-row items-center">
         <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
-        <span className="text-blue-800  font-bold text-xl">MoneySenseAI</span>
+        <span className="text-blue-800 font-bold text-xl">MoneySenseAI</span>
       </div>
-      {isSignedIn ? (
-        <UserButton />
-      ) : (
-        <div className="flex gap-3  items-center">
-          <Link href={"/dashboard"}>
-            <Button variant="outline" className="rounded-full">
-              Dashboard
-            </Button>
-          </Link>
+      <div className="flex gap-3 items-center">
+        <Link href={"/dashboard"}>
+          <Button variant="outline" className="rounded-full">
+            Dashboard
+          </Button>
+        </Link>
+        {isSignedIn ? (
+          <UserButton />
+        ) : (
           <Link href={"/sign-in"}>
             <Button className="rounded-full">Get Started</Button>
           </Link>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
