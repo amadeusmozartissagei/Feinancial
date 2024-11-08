d8ce5844810255e90ada86cd1fe22cc9b21e7be0
hamza <hamzapratama000@gmail.com>
2024-11-08 23:08:25 +0700

    update gemini model

--- utils/getFinancialAdvice.js
+++ utils/getFinancialAdvice.js
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 const getFinancialAdvice = async (totalBudget, totalIncome, totalSpend) => {
   console.log(totalBudget, totalIncome, totalSpend);
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     
     const userPrompt = `
       Based on the following financial data:
